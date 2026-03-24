44736306f2bd84f5541f084f3c4fc044129b044c
iapeTEC <bruno.souza4@eaportal.org>
2026-03-24 00:03:27 -0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyLYCok2SWRGXn5NvV2raAGLTYXJ7EaNNzqZ9jvRTp9KRYurShcWQ5cKSCw3_zSPc5h/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxX56jTq98ADW3t3TwuGcSy8oFHTcaWTxWJGTE3tBedrV41vuKIMxIRY1MVkWvQe7-p/exec',
   APP_NAME: 'IAPE Trainee',
   LOGO_URL: 'images/logo.png',
   DEFAULT_PHOTO: '',
