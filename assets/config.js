3795568c38330b98c06e468b98d9a36bd2edf8e5
iapeTEC <bruno.souza4@eaportal.org>
2026-03-24 00:01:47 -0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbyLYCok2SWRGXn5NvV2raAGLTYXJ7EaNNzqZ9jvRTp9KRYurShcWQ5cKSCw3_zSPc5h/exec',
   APP_NAME: 'IAPE Trainee',
-  LOGO_URL: '',
+  LOGO_URL: 'images/logo.png',
   DEFAULT_PHOTO: '',
   BRAND_EMAIL: 'iape.trainee@adventistas.org'
 };
