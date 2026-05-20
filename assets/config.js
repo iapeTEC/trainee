04b041755b78a29e562f79611c175dfab2360950
IAPE <bruno.souza4@eaportal.org>
2026-05-20 12:22:44 -0300

    Update API_URL in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxpERzRG3cM00VFQQVQOtBRyfRIoOegoJ5WCheFXOA_6GltGWGCFxbQmRbceVoMJCc9/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxdhhBrbOrAkbmaM2bzF5Ln0Xp4XZSH_babK5FIN1kmUVeLDpcCAdbmbigU_-shQJRg/exec',
   APP_NAME: 'IAPE Trainee',
   LOGO_URL: 'images/logo.png',
   DEFAULT_PHOTO: 'images/photo_default.png',
